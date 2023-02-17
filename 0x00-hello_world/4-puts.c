3d58539a06c8ed1bed8d77eb51911305413e69fa
NjungeSammy <samwelnjunge@gmail.com>
2023-02-16 18:46:13 -0800

    A c program that prints a line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.header>
+/**
+ * main - A program to print a line with a function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
